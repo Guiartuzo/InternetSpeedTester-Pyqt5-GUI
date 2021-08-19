8da9dfe8116119c0329c0779a912795dec95391b
Guilherme <guilherme.artuzo@hotmail.com>
2021-08-19 18:55:53 -0300

    implementação de mudança para pyqt5

--- internet_test.py
+++ internet_test.py
@@ -1,15 +1,24 @@
 import time
 import results_plotter
 from datetime import datetime
+import speedtest
 
 class InternetTest:
+    
+    st = speedtest.Speedtest()
 
     headers = ['time']
     data = []
     results = results_plotter.Writer()
 
     def Test(self, download, ping, upload, frequency):
-    
+        
+        print("iniciando tester...")
+        print(download)
+        print(ping)
+        print(upload)
+        print(frequency)
+
         while True:
 
             if download == False and upload == False and ping == False:
@@ -19,23 +28,25 @@ class InternetTest:
             current_time = time.strftime("%H:%M %d-%m-%y")
             self.data.append(current_time)
 
-            from speedtest import Speedtest
-            st = Speedtest()
 
             if download:  
                 self.headers.append("download")
-                self.data.append(st.download()/1000000)
+                self.data.append(self.st.download()/1000000)
     
             if upload: 
                 self.headers.append("upload")
-                self.data.append(st.upload()/1000000)
+                self.data.append(self.st.upload()/1000000)
 
             if ping:  
                 self.headers.append("ping")
                 servernames =[]  
-                st.get_servers(servernames)
-                self.data.append(st.results.ping)  
+                self.st.get_servers(servernames)
+                self.data.append(self.st.results.ping)  
 
             self.results.WriteFile(self.headers,self.data)
             self.data = []
             time.sleep(int(frequency)*60)
+
+if __name__ == "__main__":
+    internet = InternetTest()
+    internet.Test(True, True, True, "50")
